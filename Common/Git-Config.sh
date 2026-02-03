1e6eaa707c589d9a10c2d16957660f691a84c779
Mornye1517 <mornye150190@gmail.com>
2026-02-03 20:25:12 +0000

    build clang 23

--- Common/Git-Config.sh
+++ Common/Git-Config.sh
@@ -18,5 +18,5 @@ echo "==========================================================================
 echo "Add Git Config"
 echo "=============================================================================="
 
-git config --global user.name "mizuenaAlt"
-git config --global user.email "enashinonome264@gmail.com"
+git config --global user.name "mornye1517"
+git config --global user.email "mornye150190@gmail.com"
--- Common/clang.sh
+++ Common/clang.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone --recursive --depth=1 https://github.com/KanariaAlt/tc-build $(pwd)/llvmTC -b main
+git clone --recursive --depth=1 https://github.com/ClangBuiltLinux/tc-build $(pwd)/llvmTC -b main
 cd $(pwd)/llvmTC
 
 bash ci.sh deps
@@ -16,6 +16,7 @@ err() {
 # Environment Config
 export BRANCH=main
 export CACHE=1
+export TZ=Asia/Jakarta
 
 # Get home directory
 DIR="$(pwd ...)"
@@ -36,11 +37,11 @@ chmod +x build-llvm.py
     --quiet-cmake \
     --shallow-clone \
     --targets ARM AArch64 X86 \
-    --ref "release/18.x" \
+    --ref "main" \
     --vendor-string "$LLVM_NAME" 2>&1 | tee build.log
 
 # Check if the final clang binary exists or not.
-[ ! -f install/bin/clang-1* ] && {
+[ ! -f install/bin/clang-* ] && {
 	err "Building LLVM failed ! Kindly check errors !!"
 	err "build.log" "Error Log"
 	exit 1
@@ -68,7 +69,7 @@ for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | gre
 done
 
 # Git config
-wget https://raw.githubusercontent.com/KanariaAlt/Android-CI/main/Common/Git-Config.sh
+wget https://raw.githubusercontent.com/mornye1517/Android-CI/main/Common/Git-Config.sh
 bash Git-Config.sh
 
 # Release Info
@@ -83,30 +84,19 @@ clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
 # Push to GitHub
 # Update Git repository
-git clone "https://KanariaAlt:$GH_TOKEN@github.com/KanariaAlt/Nightcord_Clang" rel_repo
+git clone "https://mornye1517:$GH_TOKEN@github.com/mornye1517/mornye_clang" rel_repo
 pushd rel_repo || exit
 rm -fr ./*
 cp -r ../install/* .
 git lfs install
-git lfs track "clang-18"
-git lfs track "opt"
-git lfs track "clang-linker-wrapper"
 git lfs track "clang-repl"
-git lfs track "llc"
-git lfs track "llvm-lto2"
-git lfs track "llvm-lto"
-git lfs track "libLTO.so"
-git lfs track "bugpoint"
-git lfs track "clang-scan-deps"
-git lfs track "lld"
-git lfs track "libclang.so.18.1.8"
-git lfs track "libclang-cpp.so.18.1"
+git lfs track "libclang-cpp.so.23.0git"
 git checkout README.md # keep this as it's not part of the toolchain itself
 git add .
-git commit -asm "Nightcord: Update to $rel_date build
+git commit -asm "Mornye: Update to $rel_date build
 LLVM commit: $llvm_commit_url
 Clang Version: $clang_version
 Binutils version: $binutils_ver
-Builder commit: https://github.com/KanariaAlt/Nightcord_Clang/commit/$builder_commit"
-git push 
+Builder commit: https://github.com/mornye1517/mornye_clang/commit/$builder_commit"
+git push -f
 popd || exit
